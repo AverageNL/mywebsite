762806c0a68135f024b7280a54af2b0bd77a2ea0
AverageNL <60486648+AverageNL@users.noreply.github.com>
2020-03-31 13:20:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-		function HideContent(d) {
-		document.getElementById(d).style.display = "none";
-		}
-		function ShowContent(d) {
-		document.getElementById(d).style.display = "block";
-		}
-		function ReverseDisplay(d) {
-		if(document.getElementById(d).style.display == "none") { document.getElementById(d).style.display = "block"; }
-		else { document.getElementById(d).style.display = "none"; }
-		}
+$(document).ready(function(){
+  $("#hover-area").click(function(){
+    $("#reveal").slideToggle(300);
+  });
+});
